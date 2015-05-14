573cb3aa68665b261fe0a0c4189e29a82fc0c37e
Jacob Powers <powersjcb@gmail.com>
2015-05-14 14:51:14 -0700

    Create controllers for bands, tracks, and albums

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+
+  ### USER AUTH ###
   resources :users, only: [:show, :create, :new]
   resource :sessions, only: [:create, :destroy, :new]
+
+
+  ### MUSIC APP ###
+  resources :bands do
+    resources :albums, only: :index
+  end
+
+  resources :albums, except: :index do
+    resources :tracks, only: :index
+  end
+
+  resources :tracks, except: :index
 end
